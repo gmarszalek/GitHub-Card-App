0c365ab57179c47d5bca3083620ff6756e89d1a9
Grzegorz Marszałek <grzegorz.marszalek.programista@gmail.com>
2020-04-15 22:56:01 +0200

    Updates

---
+++ precache-manifest.19b6543b4dce74edb9f14110b7e4a6fe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a214c51a41001d45c83d56d68f11a11b",
+    "url": "/github-card-app/index.html"
+  },
+  {
+    "revision": "c6c497654862a8d1c668",
+    "url": "/github-card-app/static/css/main.071fef01.chunk.css"
+  },
+  {
+    "revision": "f8b89f6a543d5327a774",
+    "url": "/github-card-app/static/js/2.5d050af2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/github-card-app/static/js/2.5d050af2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6c497654862a8d1c668",
+    "url": "/github-card-app/static/js/main.5de8f500.chunk.js"
+  },
+  {
+    "revision": "cb6ef57432dd7acfa23a",
+    "url": "/github-card-app/static/js/runtime-main.70edce62.js"
+  }
+]);
